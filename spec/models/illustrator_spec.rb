07ca77185b65bd3eabc84ebfd2778c954b701dc6
Michał Zając <michal.zajac@gmail.com>
2016-06-09 20:24:17 +0200

    Improve Illustrator and VoiceActor specs.

--- spec/models/illustrator_spec.rb
+++ spec/models/illustrator_spec.rb
@@ -5,16 +5,25 @@ RSpec.describe Illustrator do
 
   it "can be created" do
     expect(illustrator).not_to be_nil
+    expect(illustrator).to be_a(Illustrator)
   end
 
   it "must have either first name, nickname, or last name" do
     illustrator = build(:illustrator, nickname: nil, last_name: nil)
     expect(illustrator.valid?).to be_truthy
+
     illustrator = build(:illustrator, first_name: nil, last_name: nil)
     expect(illustrator.valid?).to be_truthy
+
     illustrator = build(:illustrator, first_name: nil, nickname: nil)
     expect(illustrator.valid?).to be_truthy
+
     illustrator = build(:illustrator, first_name: nil, nickname: nil, last_name: nil)
     expect(illustrator.valid?).to be_falsey
+    expect(illustrator.errors.count).to eq(1)
+
+    illustrator = build(:illustrator, first_name: "", nickname: "", last_name: "")
+    expect(illustrator.valid?).to be_falsey
+    expect(illustrator.errors.count).to eq(1)
   end
 end
--- spec/models/voice_actor_spec.rb
+++ spec/models/voice_actor_spec.rb
@@ -5,16 +5,25 @@ RSpec.describe VoiceActor do
 
   it "can be created" do
     expect(voice_actor).not_to be_nil
+    expect(voice_actor).to be_a(VoiceActor)
   end
 
   it "must have either first name, nickname, or last name" do
     voice_actor = build(:voice_actor, nickname: nil, last_name: nil)
     expect(voice_actor.valid?).to be_truthy
+
     voice_actor = build(:voice_actor, first_name: nil, last_name: nil)
     expect(voice_actor.valid?).to be_truthy
+
     voice_actor = build(:voice_actor, first_name: nil, nickname: nil)
     expect(voice_actor.valid?).to be_truthy
+
     voice_actor = build(:voice_actor, first_name: nil, nickname: nil, last_name: nil)
     expect(voice_actor.valid?).to be_falsey
+    expect(voice_actor.errors.count).to eq(1)
+
+    voice_actor = build(:voice_actor, first_name: "", nickname: "", last_name: "")
+    expect(voice_actor.valid?).to be_falsey
+    expect(voice_actor.errors.count).to eq(1)
   end
 end

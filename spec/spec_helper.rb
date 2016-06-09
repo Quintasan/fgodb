adce4bcd38ca4ea92c1c1fce6f5880e829e7f293
Michał Zając <michal.zajac@gmail.com>
2016-06-09 20:15:07 +0200

    Add DatabaseCleaner.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,15 @@ Dir[File.expand_path(File.dirname(__FILE__) + "/../app/helpers/**/*.rb")].each(&
 RSpec.configure do |conf|
   conf.include Rack::Test::Methods
   conf.include FactoryGirl::Syntax::Methods
+  conf.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  conf.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
 
 # You can use this method to custom specify a Rack app

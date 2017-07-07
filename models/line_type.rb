67552a2c562ffc22598b41452b59edbe80cc0643
Michał Zając <michal.zajac@gmail.com>
2017-07-07 17:18:11 +0200

    Remove unncessary validations.
    God bless Sequel auto-validations plugin.

--- models/line_type.rb
+++ models/line_type.rb
@@ -2,10 +2,4 @@
 
 class LineType < Sequel::Model
   one_to_many :lines
-  def validate
-    super
-    validates_presence :name
-    validates_unique :name, allow_nil: true
-    validates_type(String, :name, allow_nil: true)
-  end
 end

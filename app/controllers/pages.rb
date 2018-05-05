c31309c27c4c0f095970edc975acbcc3043f4062
Michał Zając <michal.zajac@gmail.com>
2018-05-05 15:41:55 +0200

    Add a test endpoint

--- app/controllers/pages.rb
+++ app/controllers/pages.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
 Fgodb::App.controllers :pages do
+
+  DB = {
+    servants: [
+      {
+        name: "Arturia Pendragorn",
+        class: "Saber",
+      },
+      {
+        name: "Karna",
+        class: "Lancer",
+      }
+    ]
+  }
+
   get :home, map: "/" do
     render :index
+  end
+
+  get :test, map: "/test" do
+    content_type :json
+    logger.info "Hit from React"
+    DB.to_json
   end
 end
